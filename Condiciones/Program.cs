20d819c75f7a4609b558c933d472bed52b530a2f
manolo-sandoval <99548326+manolo-sandoval@users.noreply.github.com>
2022-02-24 22:31:27 -0600

    Add files via upload

---
+++ Condiciones/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CondicionesCondiciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroEntero = int.Parse(Console.ReadLine());
+
+            if (numeroEntero == 7)
+                Console.WriteLine("nuemroEntero es 7.");
+            else if (numeroEntero < 0)
+                Console.WriteLine("numeroEntero es negativo");
+            else 
+                Console.WriteLine("numeroEntero no es ninguno de los casos programados");
+
+        }
+    }
+}
---
+++ Using/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Sentencias
+{
+    class SentenciaUsing
+    {
+        public static void Main (string[] args)
+        {
+            string s;
+
+            using (TextWriter w = File.CreateText("log.txt"))
+            {
+                w.WriteLine("linea uno");
+                w.WriteLine("linea dos");
+            }
+
+            using (TextReader reader=File.OpenText("lon.txt"))
+            {
+                while((s=reader.ReadLine()) !=null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+        }
+    }
+}
+
